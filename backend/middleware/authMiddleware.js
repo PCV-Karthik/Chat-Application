bc867875b71cbd1cba134b46702d5052f5b466dc
Pandi Chandra Vadhana Karthik <76482854+PCV-Karthik@users.noreply.github.com>
2024-01-13 02:55:16 +0530

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -4,8 +4,6 @@ const asyncHandler = require("express-async-handler");
 
 const protect = asyncHandler(async (req, res, next) => {
   let token;
-  console.log(`request in protect module ${req.header}`);
-
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
